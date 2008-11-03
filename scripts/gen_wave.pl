607b66b25cd8034561688a0e05e5b8932c82979e
Matt Shannon <matt.shannon@cantab.net>
2008-11-03 19:02:21 +0000

    scripts - gen_wave.pl - fix to be suitable for STRAIGHT demo
    darcs-hash:20081103190221-3a4db-6dbc4e92633dac71ff354fbcf1c347e7cb4537b3.gz

--- scripts/gen_wave.pl
+++ scripts/gen_wave.pl
@@ -10,6 +10,10 @@ if (@ARGV != 2) {
 require($ARGV[0]);
 
 
+# File locations =========================
+# data directory
+$datdir = "$prjdir/data";
+
 
 # =============================================================
 # ===================== Main Program ==========================
@@ -29,7 +33,7 @@ sub shell($) {
 }
 
 # sub routine for log f0 -> f0 conversion
-sub lf02pitch($$) {
+sub lf02f0($$) {
    my($base,$gendir) = @_;
    my($t,$T,$data);
 
@@ -39,22 +43,24 @@ sub lf02pitch($$) {
    read(IN,$data,$STAT[7]);
    close(IN);
 
-   # log f0 -> pitch conversion
+   # log f0 -> f0 conversion
    $T = $STAT[7]/4;
    @frq = unpack("f$T",$data);
    for ($t=0; $t<$T; $t++) {
       if ($frq[$t] == -1.0e+10) {
          $out[$t] = 0.0;
       } else {
-         $out[$t] = $sr/exp($frq[$t]);
+         $out[$t] = exp($frq[$t]);
       }
    }
    $data = pack("f$T",@out);
 
    # output data
-   open(OUT,">$gendir/${base}.pit");
+   open(OUT,">$gendir/${base}.f0");
    print OUT $data;
    close(OUT);
+   
+   return $T;
 }
 
 # sub routine for formant emphasis in Mel-cepstral domain
@@ -105,22 +111,34 @@ sub postfiltering($$) {
 # sub routine for speech synthesis from log f0 and Mel-cepstral coefficients 
 sub gen_wave($) {
    my($gendir) = @_;
-   my($line,@FILE,$num,$period,$file,$base);
+   my($line,@FILE,$num,$period,$file,$base,$T,$endian);
 
    $line   = `ls $gendir/*.mgc`;
    @FILE   = split('\n',$line);
    $num    = @FILE;
    $lgopt = "-l" if ($lg);
 
-   print "Processing directory $gendir:\n";  
+   print "Processing directory $gendir:\n";
+   
+   # synthesize a waveform STRAIGHT
+   open(SYN, ">$datdir/scripts/synthesis.m") || die "Cannot open $!";
+   printf SYN "path(path,'%s');\n", ${STRAIGHT};
+   printf SYN "prm.spectralUpdateInterval = %f;\n\n", 1000.0*$fs/$sr;
+   if ($bs==0) {
+      $endian = "ieee-le";
+   }
+   else {
+      $endian = "ieee-be";
+   }
+
    foreach $file (@FILE) {
       $base = `basename $file .mgc`;
       chomp($base);
       if ( -s $file && -s "$gendir/$base.lf0" ) {
-         print " Synthesizing a speech waveform from $base.mgc and $base.lf0...";
+         print " Converting $base.mgc, $base.lf0, and $base.bap to STRAIGHT params...";
          
          # convert log F0 to pitch
-         lf02pitch($base,$gendir);
+         $T = lf02f0($base,$gendir);
          
          if ($ul) {
             # MGC-LSPs -> MGC coefficients
@@ -143,16 +161,46 @@ sub gen_wave($) {
             }
          }
          
-         # synthesize waveform
-         $line = "$EXCITE -p $fs $gendir/$base.pit | "
-               . "$MGLSADF -m ".($ordr{'mgc'}-1)." -p $fs -a $fw -g $gm $mgc | "
-               . "$X2X +fs | "
-               . "$SOX -c 1 -s -w -t raw -r $sr - -c 1 -s -w -t wav -r $sr $gendir/$base.wav";
+         # convert mgc to spectra
+         shell("$MGC2SP -a $fw -g $gm -m ".($ordr{'mgc'}-1)." -l 1024 -o 2 $mgc > $gendir/$base.sp");
 
-         shell($line);
+         # convert band-aperiodicity to aperiodicity
+         $bap = "$gendir/$base.bap";
+         shell("$BCP +f -l 5 -L 1 -s 0 -e 0 -S 0 $bap | ${DFS} -b 1 -1 | ${INTERPOLATE} -p  64 | ${DFS} -a 1 -1 > $gendir/$base.ap1");
+         shell("$BCP +f -l 5 -L 1 -s 1 -e 1 -S 0 $bap | ${DFS} -b 1 -1 | ${INTERPOLATE} -p  64 | ${DFS} -a 1 -1 > $gendir/$base.ap2");
+         shell("$BCP +f -l 5 -L 1 -s 2 -e 2 -S 0 $bap | ${DFS} -b 1 -1 | ${INTERPOLATE} -p 128 | ${DFS} -a 1 -1 > $gendir/$base.ap3");
+         shell("$BCP +f -l 5 -L 1 -s 3 -e 3 -S 0 $bap | ${DFS} -b 1 -1 | ${INTERPOLATE} -p 128 | ${DFS} -a 1 -1 > $gendir/$base.ap4");
+         shell("$BCP +f -l 5 -L 1 -s 4 -e 4 -S 0 $bap | ${DFS} -b 1 -1 | ${INTERPOLATE} -p 129 | ${DFS} -a 1 -1 > $gendir/$base.ap5");
+
+         $line = "$MERGE -s   0 -l  64 -L  64 $gendir/$base.ap1 $gendir/$base.ap2 | "
+               . "$MERGE -s 128 -l 128 -L 128 $gendir/$base.ap3 | "
+               . "$MERGE -s 256 -l 256 -L 128 $gendir/$base.ap4 | "
+               . "$MERGE -s 384 -l 384 -L 129 $gendir/$base.ap5 > $gendir/$base.ap";
+         shell($line); 
          
+         printf SYN "fprintf(1,'Synthesizing %s');\n",   "$gendir/$base.wav";
+         printf SYN "fid1 = fopen('%s','r','%s');\n", "$gendir/$base.sp", $endian;
+         printf SYN "fid2 = fopen('%s','r','%s');\n", "$gendir/$base.ap", $endian;
+         printf SYN "fid3 = fopen('%s','r','%s');\n", "$gendir/$base.f0", $endian;
+
+         printf SYN "sp = fread(fid1,[%d, %d],'float');\n", 513, $T;
+         printf SYN "ap = fread(fid2,[%d, %d],'float');\n", 513, $T;
+         printf SYN "f0 = fread(fid3,[%d, %d],'float');\n", 1,   $T;
+
+         print  SYN  "fclose(fid1);\n";
+         print  SYN  "fclose(fid2);\n";
+         print  SYN  "fclose(fid3);\n";
+
+         printf SYN "[sy] = exstraightsynth(f0,sp,ap,%d,prm);\n", $sr;
+         printf SYN "wavwrite( sy/max(abs(sy)), %d, '%s');\n\n",  $sr, "$gendir/$base.wav";
+                  
          print "done\n";
       }
    }
+   printf SYN "quit;\n";
+   close(SYN);
+   
+   print "Synthesizing waveform from STRAIGHT parameters...\n";
+   shell("$MATLAB < $datdir/scripts/synthesis.m");
    print "done\n";
 }
